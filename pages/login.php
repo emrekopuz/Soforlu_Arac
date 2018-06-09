4fa02ef7794078ab54aa11fd876cf63fb0339a20
emrekopuz <emrekopuz95@gmail.com>
2018-06-09 19:07:24 +0300

    12345

--- pages/login.php
+++ pages/login.php
@@ -29,16 +29,14 @@
         <p class="login-box-msg">Sign in to start your session</p>
         <form action="loginback.php" method="post">
           <div class="form-group has-feedback">
-            <input type="text" class="form-control" placeholder="Email" name="user"/>
+            <input type="text" class="form-control" placeholder="Kullanıcı Adı" name="user"/>
             <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
           </div>
           <div class="form-group has-feedback">
-            <input type="password" class="form-control" placeholder="Password" name="password"/>
+            <input type="password" class="form-control" placeholder="Şifre" name="password"/>
             <span class="glyphicon glyphicon-lock form-control-feedback"></span>
           </div>
           <div class="row">
-            <div class="col-xs-8">                    
-            </div><!-- /.col -->
             <div class="col-xs-4">
               <button type="submit" class="btn btn-primary btn-block btn-flat" name"sign">Sign In</button>
             </div><!-- /.col -->
